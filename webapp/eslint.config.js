81fed5f0032c0a032b6981ea2b1edf6b06626bf3
soonbee <soonbee@dudaji.com>
2026-02-05 18:30:07 +0900

    Fix eslint import order conflict for @/ path alias

--- webapp/eslint.config.js
+++ webapp/eslint.config.js
@@ -35,6 +35,15 @@ export default defineConfigWithVueTs(
                 'error',
                 {
                     groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+                    pathGroups: [
+                        {
+                            pattern: '@/**',
+                            group: 'internal',
+                            position: 'before',
+                        },
+                    ],
+                    pathGroupsExcludedImportTypes: ['builtin'],
+                    'newlines-between': 'always',
                     alphabetize: {
                         order: 'asc',
                         caseInsensitive: true,
